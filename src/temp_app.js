87af34d3c737e60cd9eb1b04db6f4daf0776b888
bfs-jy <jy@cn.fujitsu.com>
2019-11-05 19:29:29 +0800

    Module 03 - Handling Form Data

--- src/temp_app.js
+++ src/temp_app.js
@@ -3,8 +3,6 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -36,25 +34,15 @@ app.get('/credit', (req, res) => {
 
 app.get('/transfer', (req, res) => res.render('transfer'));
 app.post('/transfer', (req, res) => {
-  console.log(req.body);
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  fs.writeFilesync(path.join(__dirname, 'json/accounts. json'), accountsJSON, 'utf8');
   res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
-app.post('/payment', (req, res) =>{
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-  res.render( 'payment', { message: 'Payment Successful', account: accounts.credit });
-});
+};
 
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
 });
 
-app.listen(3000, () => console.log('PS Project Running on port 3000!'));
+app.listen(3000);
